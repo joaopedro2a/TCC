fea11fc4b1f3ff853685e7fe9fb8cc43f0124ca4
Felipe-Scandiuzzi <46937819+Felipe-Scandiuzzi@users.noreply.github.com>
2021-06-24 20:23:09 -0300

    Update Tables Webscrapping.py
    Essa versão é a mais próxima do que temos do webscrapping final. Há duas checagens a serem feitas:
    
    - Na temporada 16/17, há erros nos links que a LNB disponibilizou (pra 4 partidas)... temos que ou colocar um try except ou excluir essas partidas da análise (como fiz)
    - Precisamos arrumar uma forma melhor de referenciar a tabela de estatísticas do time da casa e do time fora. Do jeito que está agora, chamando índice de lista, não temos confiança na resposta. Ideal seria conseguir pegar pela classe!

--- Tables Webscrapping.py
+++ Tables Webscrapping.py
@@ -2,22 +2,134 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import requests
 
-url="https://lnb.com.br/noticias/flamengo-86-x-79-unifacisa/"
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+import re
 
-page=requests.get(url)
-soup=BeautifulSoup(page.text,'html.parser')
-table=soup.find_all('table')
+'''
+o url de cada página de tabela de jogos é formado por duas partes, separadas pelo
+código de cada temporada
+'''
 
-home=table[0]
-away=table[5]
+url_parte1="https://lnb.com.br/nbb/tabela-de-jogos/?season%5B%5D="
+url_parte2="&wherePlaying=-1&played=-1"
 
-results=[]
-for row in home.find_all('tr'):
-    result=[]
-    result.append(row.text.replace("\n\r\n",",").replace(" ","").replace("\n",","))
-    results.append(result)
+cod_temp={"20/21":"59",
+               "19/20":"54",
+               "18/19":"47",
+               "17/18":"41",
+               #"16/17":"34",
+               "15/16":"27",
+               "14/15":"20",
+               "13/14":"15",
+               "12/13":"8",
+               "11/12":"4",
+               "10/11":"3",
+               "09/10":"2",
+               "08/09":"1"}
+
+jogos_temp={"20/21":[],
+               "19/20":[],
+               "18/19":[],
+               "17/18":[],
+               #"16/17":[],
+               "15/16":[],
+               "14/15":[],
+               "13/14":[],
+               "12/13":[],
+               "11/12":[],
+               "10/11":[],
+               "09/10":[],
+               "08/09":[]}
+
+results_temp={"20/21":'',
+               "19/20":'',
+               "18/19":'',
+               "17/18":'',
+               #"16/17":'',
+               "15/16":'',
+               "14/15":'',
+               "13/14":'',
+               "12/13":'',
+               "11/12":'',
+               "10/11":'',
+               "09/10":'',
+               "08/09":''}
+
+df_results= pd.DataFrame()
+
+for temp in cod_temp:
+    url="https://lnb.com.br/nbb/tabela-de-jogos/?season%5B%5D="+cod_temp[temp]+"&wherePlaying=-1&played=-1"
+    page=requests.get(url)
+    soup=BeautifulSoup(page.text,'html.parser')
+    for link in soup.find_all('a',href=True):
+        if (("noticias" in link['href'] and re.search("/noticias/(.*)/$",link['href'])!=None) or ("partidas" in link['href'])) and link['href'] not in jogos_temp[temp]:
+            jogos_temp[temp].append(link['href'])
+        else:
+            pass
+    dates=soup.find_all(class_="date_value show-for-medium")
+    date=[i.text.replace("\n","")[0:10] for i in dates]
+    time=[i.text.replace("\n","")[10:15] for i in dates]
+    teams=soup.find_all(class_="team-shortname")
+    results_home=soup.find_all(class_="home")
+    results_away=soup.find_all(class_="away")
+    results_home=[i.text for i in results_home][1::2]
+    results_away=[i.text for i in results_away][1::2]
+    home_teams=[]
+    away_teams=[]
+    index=0
+    for team in teams:
+        if index%2==0:
+            home_teams.append(team.text)
+        else:
+            away_teams.append(team.text)
+        index+=1
+    df_temp = pd.DataFrame({'Season':temp,'Game Date':date,'Game Time':time,'Home Team':home_teams,
+                   'Home Score':results_home,'Away Score':results_away,
+                   'Away Team':away_teams,'Report Link':jogos_temp[temp]
+                   })
+    df_results=df_results.append(df_temp)
     
-df = pd.DataFrame(results,columns=['Full Data'])
-df[['TBD','Nr.','Jogador','JO','Min','Pts','RD+RORT','AS','3P%','2P%','LL%','BR','TO','FC','FR','ER','EN','+/-','EF','TBD2']] = df['Full Data'].str.split(',',expand=True)
-df = df.drop(columns=['TBD', 'TBD2','Full Data'])
-df = df.iloc[1:]
+#Coleta de tabelas de Estatísticas
+
+links=[]
+df_results=df_results.reset_index(drop=True)
+
+for item in df_results.index:
+    links.append(df_results['Report Link'][item])
+
+full_df= pd.DataFrame()
+for url in links:
+    page=requests.get(url)
+    soup=BeautifulSoup(page.text,'html.parser')
+
+    table=soup.find_all('table')
+
+#essa parte temos que revisar! como pegar a tabela do time de casa e fora sem ser assim?
+    home=table[0]
+    away=table[5]
+
+    results=[]
+    for row in home.find_all('tr'):
+        result=[]
+        result.append("Home"+row.text.replace("\n\r\n",",").replace(" ","").replace("\n",","))
+        results.append(result)
+    
+
+    for row in away.find_all('tr'):
+        result=[]
+        result.append("Away"+row.text.replace("\n\r\n",",").replace(" ","").replace("\n",","))
+        results.append(result)
+    
+    df = pd.DataFrame(results,columns=['Full Data'])
+    df['URL']=url
+    full_df=full_df.append(df)
+    print("Ok")
+
+
+full_df[['Home/Away','Nr.','Jogador','JO','Min','Pts','RD+RORT','AS','3P%','2P%','LL%','BR','TO','FC','FR','ER','EN','+/-','EF','TBD2']] = df['Full Data'].str.split(',',expand=True)
+full_df = full_df.drop(columns=['TBD2','Full Data'])
+full_df=full_df[(df['Nr.'] != 'Nr.')]
+full_df=full_df.reset_index(drop=True)
+full_df.to_csv("C:\\Users\\fcastro\\OneDrive - Digicorner\\TCC\\BRILHAMOS.csv")
